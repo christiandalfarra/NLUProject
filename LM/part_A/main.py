4be3cc6721b1e2afb0060dcf6a71ae1913ec9adf
chrid <chridf11@gmail.com>
2025-04-01 18:24:37 +0200

    changes in test

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -6,8 +6,7 @@ from functions import *
 if __name__ == "__main__":
     hid_size = 200
     emb_size = 300
-    lr=0.0001
-    lr_values = [0.0001, 0.001, 0.01]
+    lr_values = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
     clip = 5
     n_epochs = 100
     patience = 3
@@ -20,4 +19,6 @@ if __name__ == "__main__":
     plt.xlabel('Learning Rate')
     plt.ylabel('PPL')
     plt.title('PPL vs Learning Rate')
+    #plt.savefig('plot.png', dpi=300) to save the plot use this command
+    #organizzati le varie iterazioni con i parametri che vuoi valutare
     plt.show()
