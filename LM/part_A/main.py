f3a3db60e55005086a5c9c5b286d0af8d72c6469
chrid <chridf11@gmail.com>
2025-04-07 20:39:19 +0200

    no mex

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
     hid_size = 200
     emb_size = 300
     lr_values = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
+    lr = 0.5
     clip = 5
     n_epochs = 100
     patience = 3
     results = []
-    for lr in lr_values:
+    result = training(hid_size, emb_size, lr, clip, n_epochs, patience)
+    """ for lr in lr_values:
         print(f"Training with lr: {lr}")
         results = training(hid_size, emb_size, lr, clip, n_epochs, patience)
     print(f"Results: {results}")
@@ -19,7 +21,7 @@ if __name__ == "__main__":
     plt.xlabel('Learning Rate')
     plt.ylabel('PPL')
     plt.title('PPL vs Learning Rate')
-    plt.savefig('plots/plot.png', dpi=300) #to save the plot use this command
+    plt.savefig('plots/plot.png', dpi=300) """ #to save the plot use this command
     #organizzati le varie iterazioni con i parametri che vuoi valutare
     #primo test fino a 0.05 sta sopra i 250, da 0.05 compreso in poi scende sotto i 200 con 100 epoche
     #usando RNN
