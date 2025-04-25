6d78423b56a1212a1bc5effc2a76772408e4265d
christiandalfarra <christian.dalfarra@unitn.it>
2025-04-25 10:18:56 +0200

    some tests

--- LM/part_A/test_loading_weights.py
+++ LM/part_A/test_loading_weights.py
@@ -14,21 +14,24 @@ def testing(emb_size, hid_size, vocab_len, pad_index, model_path, train_loader, 
     # Set to evaluation mode (if inferencing)
     model.eval()
     print('Model loaded and set to evaluation mode.')
-
     criterion_eval = nn.CrossEntropyLoss(ignore_index=pad_index, reduction='sum')
-
     final_ppl,  _ = eval_loop(test_loader, criterion_eval, model)
-
     print('Test ppl: ', final_ppl)
-    
-    
-emb_size = 300     # example value
-hid_size = 200 
 
 train_loader, dev_loader, test_loader, lang = getLoaders()
 vocab_len = len(lang.word2id)
+
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.001.pt', train_loader, dev_loader, test_loader)
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.005.pt', train_loader, dev_loader, test_loader)
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.01.pt', train_loader, dev_loader, test_loader)
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.05.pt', train_loader, dev_loader, test_loader)
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.1.pt', train_loader, dev_loader, test_loader)
+testing(300, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_lr_0.5.pt', train_loader, dev_loader, test_loader)
 
 testing(350, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize350_hidsize200.pt', train_loader, dev_loader, test_loader)
 testing(250, 200, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize250_hidsize200.pt', train_loader, dev_loader, test_loader)
 testing(300, 150, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize300_hidsize150.pt', train_loader, dev_loader, test_loader)
 testing(300, 250, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize300_hidsize250.pt', train_loader, dev_loader, test_loader)
+
+testing(300, 250, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize300_hidsize250_lr0.05.pt', train_loader, dev_loader, test_loader)
+testing(350, 250, vocab_len, lang.word2id['<pad>'], 'bin/RNN_embsize350_hidsize250_lr0.05.pt', train_loader, dev_loader, test_loader)
