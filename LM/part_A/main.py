cb81d2f6739f06c863388ecc5d1c8d47831e7136
chrid <chridf11@gmail.com>
2025-09-11 12:00:26 +0200

    retrain for the LSTM for some problems with the saved weights

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     hid_size = 350
     emb_size = 350
     lrs = [0.1, 0.5]
-    i = 2
+    i = 3
     for lr in lrs:
         results = training_SGD(hid_size, emb_size, lr, clip = 5 , n_epochs = 100 , patience = 3 , experiment=f'exp{i}_LSTM_embsize{emb_size}_hidsize{hid_size}_lr{lr}')
         i+=1
