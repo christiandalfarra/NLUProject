9e6f98a553ac4078a0b22699e6408cd16fb4277d
chrid <chridf11@gmail.com>
2026-01-16 11:56:44 +0100

    exp

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -19,10 +19,10 @@ from functions import *
 param ={
     'mode': 'train',  # 'train' or 'evaluate'
 
-    'model_arch' : 'LSTM_DOEMB_LAST_LAYER',
+    'model_arch' : 'LSTM_DOEMB_LAYER',
     'emb_size': 350,
     'hidden_size': 350,
-    'emb_dropout': 0.2,
+    'emb_dropout': 0.5,
     'out_dropout': 0.2,
 
     'lr' : 0.5,
