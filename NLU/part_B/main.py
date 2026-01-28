cc922ef2e65edd17679434ad3de440bf0e991e83
chrid <chridf11@gmail.com>
2026-01-28 17:51:19 +0100

    fix code

--- NLU/part_B/main.py
+++ NLU/part_B/main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument('--mode', type=str, default=None, help='train or inference')
     parser.add_argument('--lr', type=float, default=None, help='learning rate')
     parser.add_argument('--optimizer', type=str, default='Adam', help='SGD, AdamW, or Adam')
-    parser.add_argument('--dropout_prob', type=float, default=None, help='dropout probability')
+    parser.add_argument('--dropout_prob', type=float, default=0.0, help='dropout probability')
 
     args = parser.parse_args()
 
@@ -41,8 +41,7 @@ if __name__ == "__main__":
 
         param['lr'] = args.lr
         param['optimizer'] = args.optimizer
-        if args.dropout_prob is not None:
-            param['dropout_prob'] = args.dropout_prob
+        param['dropout_prob'] = args.dropout_prob
 
         experiment = f"bert_lr{param['lr']}_{param['optimizer']}{'_DO' if param['dropout_prob'] > 0 else 'NoDO'}"
         print("Training Mode")
